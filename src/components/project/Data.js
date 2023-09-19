dc19a6b54eb6de65952c133ccdb2fdddbcf43545
santhosht1009 <santhosht1009@gmail.com>
2023-09-19 14:31:38 +0530

    data change

--- src/components/project/Data.js
+++ src/components/project/Data.js
@@ -2,49 +2,49 @@ export const projects = [
   {
     id: 1,
     title: "E-Commerce Website",
-    FrontEnd:["React","Tailwind"],
-    MiddleWare:"Java SprintBoot",
-    Backend:"MySql",
-    image:"images/ECommerce.png",
-     description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      live:'',
-      gith:"https://github.com/santhosht1009/ecommerce-frontend"
-    },
+    FrontEnd: ["React", "Tailwind"],
+    MiddleWare: "Java SprintBoot",
+    Backend: "MySql",
+    image: "images/ECommerce.png",
+    description:
+      "Built a user-friendly online store using Java and Spring Boot. Successfully implemented key functionalities including user authentication, product catalog management, shopping cart operation, and secure payment integration.Implemented a shopping cart system enabling users to select items, check prices and smoothly complete purchases",
+    live: "",
+    gith: "https://github.com/santhosht1009/ecommerce-frontend",
+  },
   {
     id: 2,
     title: "Campus Recruitment Management System",
-    FrontEnd:["HTML","CSS","JS"],
-    MiddleWare:"PHP",
-    Backend:"MySql",
-    image:"images/CRMS.png",
+    FrontEnd: ["HTML", "CSS", "JS"],
+    MiddleWare: "PHP",
+    Backend: "MySql",
+    image: "images/CRMS.png",
     description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      live:'',
-      gith:"https://github.com/santhosht1009/crms"
-    },
+      "Campus Selection System is a platform that provide interface between students and company System provides the list of suitable companies to the students, according to their educational qualification, experience and their preferences. System provides the list of eligible students from a pool according to required skill for vacancy of company. The aim is to provide services to both the Employer and the Graduate by recruiting Graduates according to the employer specifications",
+    live: "",
+    gith: "https://github.com/santhosht1009/crms",
+  },
   {
     id: 3,
     title: "Computer Service Management System",
-    FrontEnd:["HTML","CSS","JS"],
-    MiddleWare:"PHP",
-    Backend:"MySql",
-    image:"images/CSMS.png",
-     description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-  live:'',
-  gith:"https://github.com/santhosht1009/Computer-Service-Mangement-System"
-    },
+    FrontEnd: ["HTML", "CSS", "JS"],
+    MiddleWare: "PHP",
+    Backend: "MySql",
+    image: "images/CSMS.png",
+    description:
+      "A web portal through user can request for service and the company can assign engineers for the service that requested by the user. The Computer Service Management is the primary means of ensuring a consistent and appropriate level of quality for all services",
+    live: "",
+    gith: "https://github.com/santhosht1009/Computer-Service-Mangement-System",
+  },
   {
     id: 4,
     title: "Portfolio",
-    FrontEnd:["React","Tailwind","BootStrap"],
-    MiddleWare:"",
-    Backend:"",
-    image:"images/Portfolio.png",
-     description:
+    FrontEnd: ["React", "Tailwind", "BootStrap"],
+    MiddleWare: "",
+    Backend: "",
+    image: "images/Portfolio.png",
+    description:
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      live:'true',
-      gith:"https://github.com/santhosht1009/portfolio"
-    },
+    live: "true",
+    gith: "https://github.com/santhosht1009/portfolio",
+  },
 ];
